4009880b96660d2b8627ffcc40dc4137aa67c27f
Achraf Kassioui <achrafkassioui@gmail.com>
2023-10-30 23:11:41 +0100

    Initial Commit

---
+++ CoordinateConversion/CoordinateConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinateConversionApp.swift
+//  CoordinateConversion
+//
+//  Created by Achraf Kassioui on 30/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoordinateConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
